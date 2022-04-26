4a89f075c7550e10acb676ea13a38baee71a3fd3
Bhakti Chaudhari <bhakti.chaudhari96@gmail.com>
2022-04-26 16:23:39 -0400

    cache insertion pass for AdminInfoController in train ticket

--- CacheInsertionPass.java
+++ CacheInsertionPass.java
@@ -1,15 +1,12 @@
 
-//package dev.navids.soottutorial.helloworldanalysis;
-import java.io.File;
 import java.util.*;
 
-import fj.data.Array;
 import soot.*;
 import soot.G;
 import soot.Scene;
 import soot.options.Options;
-import soot.jimple.JimpleBody;
-import soot.jimple.toolkits.callgraph.*;
+import soot.jimple.*;
+import soot.jimple.internal.*;
 
 public class CacheInsertionPass {
     public static void setupSoot(String sourceDirectory, String[] className, String methodName) {
@@ -18,24 +15,43 @@ public class CacheInsertionPass {
         Options.v().set_prepend_classpath(true);
         Options.v().set_allow_phantom_refs(true);
         Options.v().set_soot_classpath(sourceDirectory);
-        // for (String sd : sourceDirectory) {
-        // System.out.println(sd);
-        // // Scene.v().extendSootClassPath(sd);
-        // Options.v().set_soot_classpath(sd);
-        // }
         Options.v().set_whole_program(true);
-        // ArrayList<String> jars = new ArrayList<String>();
-        // jars.add(sourceDirectory);
-        // Options.v().set_process_jar_dir(jars);
-        // Scene.v().extendSootClassPath(sourceDirectory);
         for (String cls : className) {
-            // System.out.println(cls);
             SootClass sc = Scene.v().loadClassAndSupport(cls);
             sc.setApplicationClass();
         }
         Scene.v().loadNecessaryClasses();
+    }
 
-        // System.out.println(Scene.v().getClasses());
+    public static void addCacheField(SootClass cls) {
+
+        SootClass cacheClass = Scene.v().getSootClass("Cache");
+        Local cacheLocal = Jimple.v().newLocal("cacheLocal", cacheClass.getType());
+        Value invokeCacheInit = Jimple.v().newSpecialInvokeExpr(cacheLocal,
+                cacheClass.getMethodByName("<init>").makeRef());
+        Unit assg1 = Jimple.v().newAssignStmt(cacheLocal, invokeCacheInit);
+        Value staticCache = Jimple.v().newStaticFieldRef(cls.getFieldByName("cache").makeRef());
+        Unit assg2 = Jimple.v().newAssignStmt(staticCache, cacheLocal);
+        SootMethod init = cls.getMethodByName("<clinit>");
+        System.out.println(init.retrieveActiveBody());
+        // System.out.println(cls.getMethodByName("<init>").retrieveActiveBody());
+        JimpleBody body = (JimpleBody) init.retrieveActiveBody();
+        Iterator it = body.getUnits().iterator();
+        Unit u = null;
+        while (it.hasNext()) {
+            u = (Unit) it.next();
+            if (u instanceof AssignStmt) {
+                AssignStmt as = (AssignStmt) u;
+                if (as.getLeftOp() instanceof StaticFieldRef) {
+                    System.out.println("static field - " + as.getLeftOp());
+                }
+            }
+        }
+        body.getLocals().add(cacheLocal);
+        body.getUnits().addFirst(assg2);
+        body.getUnits().addFirst(assg1);
+        System.out.println(init.retrieveActiveBody());
+        body.validate();
     }
 
     public static void main(String[] args) {
@@ -44,29 +60,58 @@ public class CacheInsertionPass {
         String methodName = args[2];
         setupSoot(sourceDirectory, className, methodName);
 
-        // Retrieve HelloWorld's body
         SootClass mainClass = Scene.v().getSootClass(className[0]);
-        // mainClass.setApplicationClass();
-        SootMethod sm = mainClass.getMethodByName("getAllContacts");
+        SootClass cacheClass = Scene.v().getSootClass("Cache");
+        SootField f = Scene.v().makeSootField("cache", cacheClass.getType(), Modifier.STATIC);
+        mainClass.addField(f);
+        mainClass.setApplicationClass();
 
-        ArrayList<SootMethod> entryPoints = new ArrayList<SootMethod>();
-        for (SootClass currentClass : Scene.v().getClasses()) {
-            for (SootMethod method : currentClass.getMethods()) {
-                // System.out.println(method.getName());
-                entryPoints.add(method);
+        addCacheField(mainClass);
+        SootMethod sm = mainClass.getMethodByName("getAllContacts");
+        JimpleBody body = (JimpleBody) sm.retrieveActiveBody();
+        Iterator it = body.getUnits().iterator();
+        Value v = null;
+        Unit toreplace = null;
+        Unit wrapRes = null;
+        Value staticCache = Jimple.v().newStaticFieldRef(mainClass.getFieldByName("cache").makeRef());
+        while (it.hasNext()) {
+            Unit ub = (Unit) it.next();
+            // identify call site
+            if (ub.toString().contains("getAllContacts(org.springframework.http.HttpHeaders)")) {
+                toreplace = ub;
+                JAssignStmt as = (JAssignStmt) ub;
+                v = as.getLeftOp();
+            }
+            // identify replacement point
+            else if (ub.toString().contains(
+                    "staticinvoke <org.springframework.http.ResponseEntity: org.springframework.http.ResponseEntity ok(java.lang.Object)>")) {
+                wrapRes = ub;
             }
         }
-        Scene.v().setEntryPoints(entryPoints);
-        CHATransformer.v().transform();
-        CallGraph cg = Scene.v().getCallGraph();
-        Iterator<Edge> it = cg.edgesInto(sm);
-        System.out.println("Number of edges is " + cg.size());
-        while (it.hasNext()) {
-            Edge e = it.next();
-            System.out.println(e.toString());
+        JAssignStmt as = (JAssignStmt) toreplace;
+        SootMethod cacheGet = cacheClass.getMethodByName("get");
+        SootMethod cachePut = cacheClass.getMethodByName("put");
+        Local cacheLocal = Jimple.v().newLocal("cacheLocal", cacheClass.getType());
+        body.getLocals().add(cacheLocal);
+        Value newub = Jimple.v().newVirtualInvokeExpr(cacheLocal, cacheGet.makeRef(),
+                as.getInvokeExpr().getArgs());
+        AssignStmt assg = Jimple.v().newAssignStmt(v, newub);
+        body = (JimpleBody) sm.retrieveActiveBody();
+        body.getUnits().insertBefore(assg, toreplace);
+        NeExpr qualExpr = Jimple.v().newNeExpr(NullConstant.v(), v);
+        EqExpr qualExpr2 = Jimple.v().newEqExpr(NullConstant.v(), v);
+        Unit ifstatement = Jimple.v().newIfStmt(qualExpr, wrapRes);
+        Unit ifstatement2 = Jimple.v().newIfStmt(qualExpr2, toreplace);
+        List<Value> putArgs = as.getInvokeExpr().getArgs();
+        putArgs.add(v);
+        InvokeExpr putStmt = Jimple.v().newVirtualInvokeExpr(cacheLocal, cachePut.makeRef(),
+                putArgs);
 
-        }
-
+        body.getUnits().insertAfter(ifstatement, assg);
+        body.getUnits().insertAfter(ifstatement2, ifstatement);
+        body.getUnits().insertAfter(soot.jimple.Jimple.v().newInvokeStmt(putStmt), toreplace);
+        body = (JimpleBody) sm.retrieveActiveBody();
+        System.out.println(body);
+        body.validate();
     }
-
 }
